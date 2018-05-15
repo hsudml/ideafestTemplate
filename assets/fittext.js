3b8d2238c66c03c613224e916d80f86447c621a7
Tim Miller <twm2@humboldt.edu>
2018-05-15 09:54:57 -0700

    Create fittext.js

---
+++ assets/fittext.js
@@ -0,0 +1,8 @@
+ $('.fittext-dynamic').each(function(){
+    let t          = $(this),
+        width      = t.data("width") * 2,
+        characters = t.html().length,
+        size       = width/characters;
+    
+    $(this).css('font-size', `${size}vw`);
+  });
