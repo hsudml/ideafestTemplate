22f57ac5bf3af954d88465ce6b5383642a369d94
Tim Miller <twm2@humboldt.edu>
2018-05-15 09:55:34 -0700

    Delete fittext.js

--- assets/fittext.js
+++
@@ -1,8 +0,0 @@
- $('.fittext-dynamic').each(function(){
-    let t          = $(this),
-        width      = t.data("width") * 2,
-        characters = t.html().length,
-        size       = width/characters;
-    
-    $(this).css('font-size', `${size}vw`);
-  });
