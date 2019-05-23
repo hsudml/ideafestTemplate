eaffae656ef73ffaee0e414acdfdffc2d167914b
Tim Miller <twm2@humboldt.edu>
2019-05-23 14:58:35 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,4 @@ var title = document.getElementById('ideafest-2018').innerText;
 var backdrop = document.createElement("div");   
 backdrop.className += "backdrop";
 backdrop.innerHTML = title; 
-console.log(title);
 document.body.appendChild(backdrop);
