7929df465b1c129b67d20a2f2501eff841e24196
Tim Miller <twm2@humboldt.edu>
2019-05-23 15:50:41 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
-var title = document.getElementById('ideafest-2018').innerText;
-var backdrop = document.createElement("div");   
-backdrop.className += "backdrop";
-backdrop.innerHTML = title; 
-document.body.appendChild(backdrop);
+function backdropCreate(){
+  var title = document.getElementById('ideafest-2018').innerText;
+  var backdrop = document.createElement("div");   
+  backdrop.className += "backdrop";
+  backdrop.innerHTML = title; 
+  document.body.appendChild(backdrop);
+}
+backdropCreate();
+
+function addAnchorLink(){
+  var anchor = document.getElementsByTagName('h3');
+    for (var i = 0; i < anchor.length; i++) {
+		  var anchorLink = document.createElement("ion-icon");   
+      anchorLink.setAttribute("name","ios-link");
+      anchor[i].prepend(anchorLink);
+    }
+}
+addAnchorLink();
